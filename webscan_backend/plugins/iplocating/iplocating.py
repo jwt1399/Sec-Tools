3b774eacd3c3ed24cc051a7f9aff4ce3cb098c91
CodeLorin <byxlorin@gmail.com>
2023-05-19 22:48:14 +0800

    Update IP geolocation API,the previous API frequently returned a 429 error.

--- webscan_backend/plugins/iplocating/iplocating.py
+++ webscan_backend/plugins/iplocating/iplocating.py
@@ -15,13 +15,13 @@ def get_locating(ip):
     """
     获取ip归属地
     """
-    api_url = 'http://freeapi.ipip.net/'
+    api_url = 'http://ip-api.com/json/{}?lang=zh-CN'.format(ip)
     try:
-        res = requests.get(api_url+ip, timeout=4)
-        result_str = literal_eval(res.text)
+        res = requests.get(api_url, timeout=4)
+        json_data = res.json()
         # result_str = (result[0])
         # print(result)
-        # result_str = '国家({})，省份({})，城市({})'.format(result[0],result[1],result[2])
+        result_str = '国家({})，省份({})，城市({})'.format(json_data['country'],json_data['regionName'],json_data['city'])
     except Exception as e:
         result_str = '获取数据失败，请稍后再试'
         print(result_str)
